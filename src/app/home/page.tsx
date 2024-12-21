4b5783e63a8c6f77b4834e66ba0a8d77ded1bb35
Draimonox <Draimonox@gmail.com>
2024-12-20 23:59:03 -0500

    implemented cookie check pre-render

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,16 +1,28 @@
 "use client";
 import { hasCookie } from "cookies-next";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 function MainPage() {
+  const [isAllowed, setIsAllowed] = useState(false);
   const cookieCheck = hasCookie("JWT");
 
   useEffect(() => {
     if (!cookieCheck) {
       window.location.href = "/login";
+    } else {
+      setIsAllowed(true);
     }
-  });
-  return <div>Hellow World</div>;
+  }, []);
+
+  if (!isAllowed) {
+    return null;
+  }
+
+  return (
+    <>
+      <div>Hellow World</div>;
+    </>
+  );
 }
 
 export default MainPage;
